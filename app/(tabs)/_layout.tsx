59a0b3df94e00c97f75cc90972aaf2543ce0cf31
DukeAkinOluwa <akindukoakinoluwa@gmail.com>
2025-06-12 19:25:04 +0100

    Update: Updates the styling of the bottom navigation bar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,5 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
-import { Platform } from 'react-native';
 
 import { HapticTab } from '@/components/HapticTab';
 import TabBarBackground from '@/components/ui/TabBarBackground';
@@ -9,49 +8,63 @@ import { useColorScheme } from '@/hooks/useColorScheme';
 import { FontAwesome } from '@expo/vector-icons';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
+    const theme = useColorScheme() ?? 'light';
+    const colorSet = Colors[theme];
 
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarActiveTintColor: colorSet.primary,
         headerShown: false,
         tabBarButton: HapticTab,
         tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
+        tabBarStyle: {
+          position: 'absolute',
+          height: 70,
+          paddingBottom: 10,
+          paddingTop: 5,
+          // backgroundColor: 'rgba(255, 255, 255, 0.9)',
+          borderTopLeftRadius: 20,
+          borderTopRightRadius: 20,
+          borderTopWidth: 0,
+          elevation: 10, // Android shadow
+          shadowColor: '#000', // iOS shadow
+          shadowOffset: { width: 0, height: -3 },
+          shadowOpacity: 0.1,
+          shadowRadius: 10,
+        },
+        tabBarLabelStyle: {
+          fontSize: 12,
+          fontFamily: 'Montserrat-SemiBold',
+          marginBottom: 6,
+        },
       }}>
       <Tabs.Screen
         name="index"
         options={{
           title: 'Home',
-          tabBarIcon: ({ color }) => <FontAwesome name='home' size={28} color={color} />,
+          tabBarIcon: ({ focused }) => <FontAwesome name="home" size={focused ? 30 : 24} color={focused ? colorSet.primary : 'gray'} style={{ marginBottom: 4 }} />,
         }}
       />
       <Tabs.Screen
         name="schedule"
         options={{
           title: 'Schedule',
-          tabBarIcon: ({ color }) => <FontAwesome name='calendar' size={28} color={color} />,
+          tabBarIcon: ({ focused }) => <FontAwesome name="calendar" size={focused ? 30 : 24} color={focused ? colorSet.primary : 'gray'} style={{ marginBottom: 4 }} />,
         }}
       />
       <Tabs.Screen
         name="resources"
         options={{
           title: 'Resources',
-          tabBarIcon: ({ color }) => <FontAwesome name='book' size={28} color={color} />,
+          tabBarIcon: ({ focused }) => <FontAwesome name="book" size={focused ? 30 : 24} color={focused ? colorSet.primary : 'gray'} style={{ marginBottom: 4 }} />,
         }}
       />
       <Tabs.Screen
         name="explore"
         options={{
           title: 'Explore',
-          tabBarIcon: ({ color }) => <FontAwesome name='rocket' size={28} color={color} />,
+          tabBarIcon: ({ focused }) => <FontAwesome name="plane" size={focused ? 30 : 24} color={focused ? colorSet.primary : 'gray'} style={{ marginBottom: 4 }} />,
         }}
       />
     </Tabs>
